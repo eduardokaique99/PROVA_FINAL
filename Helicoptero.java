09008be7918ba8a5c5557e46f139661f21ac6ce4
eduardokaique99 <eduardo_kaique99@hotmail.com>
2022-11-29 21:44:22 -0300

    29/11/2022

--- Helicoptero.java
+++ Helicoptero.java
@@ -1,3 +1,55 @@
+import java.util.ArrayList;
+
 public class Helicoptero extends Aeromodelo {
+        
+        private String cor;
+        private int capacidade;
+
+        private static ArrayList<Helicoptero> helicopteros = new ArrayList<>();
     
-}
+        public Helicoptero(int id, String marca, String modelo, String cor, int capacidade) {
+            super(id, marca, modelo);
+            this.cor = cor;
+            this.capacidade = capacidade;
+
+            setHelicoptero(this);
+        }
+
+        public String getCor() {
+            return cor;
+        }
+    
+        public void setCor(String cor) {
+            this.cor = cor;
+        }
+    
+        public static ArrayList<Helicoptero> getHelicoptero() {
+            return helicopteros;
+        }
+    
+        public static void setHelicoptero(Helicoptero helicoptero) {
+            helicopteros.add(helicoptero);
+        }
+    
+        @Override
+        public String toString() {
+            return super.toString() 
+                + "\ncapacidade=" + capacidade
+                + "\ncor=" + cor + "\n";
+        }
+    
+        public static Helicoptero getHelicopteroById(int id) throws Exception {
+            for (Helicoptero helicoptero : helicopteros) {
+                if (helicoptero.getId() == id) {
+                    return helicoptero;
+                }
+            }
+            throw new Exception("Helicoptero não encontrado");
+        }
+
+    
+        public static void excluir(int id) throws Exception {
+            Helicoptero helicoptero = getHelicopteroById(id);
+            helicopteros.remove(helicoptero);
+        }
+    }
